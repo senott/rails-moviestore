79c6c58f6ebeecdfed18090d0d266ebcd967cac0
Senott <senott@yahoo.com>
2015-04-04 16:59:25 -0300

    First commit

---
+++ app/models/movie.rb
@@ -0,0 +1,26 @@
+class Movie < ActiveRecord::Base
+  has_many :purchases
+  has_many :buyers, through: :purchases
+  
+  def show
+    @movie = Movie.find(params[:id])
+    @cart_action = @movie.cart_action current_user.try :id
+  end
+  
+  def poster
+    "http://ia.media-imdb.com/images/M/#{poster_url}"
+  end
+  
+  def imdb
+    "http://www.imdb.com/title/#{imdb_id}/"
+  end
+  
+  private
+    def cart_action(current_user_id)
+      if $redis.sismember "cart#{current_user_id}", id
+        "Remove from"
+      else
+        "Add to"  
+      end
+    end
+end
